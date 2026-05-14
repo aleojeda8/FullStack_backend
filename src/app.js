29962ac877e8e0dc92335eb9e3ba6210f411fb9a
aleojeda8 <aleojeda861@gmail.com>
2026-05-14 20:27:19 -0300

    Prmeros cambos

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import './config/env.js'
+
+import ConnectDB from '.config/db.js'
+
+import userRoutes from './routes/use.routes.js'
+import { connect } from 'mongoose';
+
+const app = express()
+
+app.use(express.json())
+
+ConnectDB()
+
+app.use(userRoutes)
+
+app.listen(process.env.PORT,() =>{
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
+})
